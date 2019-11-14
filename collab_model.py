c3e02c440023879c4b94be0172414857738793c7
marek-kan <56593496+marek-kan@users.noreply.github.com>
2019-11-14 09:18:33 +0100

    feature: add fit_sgd

--- collab_model.py
+++ collab_model.py
@@ -7,6 +7,7 @@ Created on Sat Oct  5 13:40:40 2019
 
 import utils
 import numpy as np
+import time
 
 class collab_model():
     
@@ -16,55 +17,33 @@ class collab_model():
         self.n_iter = n_iter
         self.n_features = n_features
     
-    
-    def fit(self, Y, R):
+
+    def fit_sgd(self, Y, R):
         n_jokes = Y.shape[0]
         n_users = Y.shape[1]
         X, Theta = utils.init_par(n_users, n_jokes, self.n_features)
-        
+        start = time.time()
         for i in range(self.n_iter):
+            
+            X, Theta = utils.sgd(X, Theta, Y, self.lamb, R, init_learning_rate=self.learning_rate, max_iter=8)
             J = utils.cost(X, Theta, Y, self.lamb, R)
-            X_grad, Theta_grad = utils.grad(X, Theta, Y, self.lamb, R)
-            
-            X = X - self.learning_rate * X_grad
-            Theta = Theta - self.learning_rate * Theta_grad
             print('cost: ' + str(J),', n_iter: '+str(i))
-            
+            if J < 200:
+                break
         self.features = X
         self.coef = Theta
         self.cost = utils.cost(X, Theta, Y, self.lamb, R)
-        print('final cost: '+ str(self.cost))
+        end = time.time()
+        self.train_time = end-start
+        print('final cost: '+ str(self.cost),'\n'
+              'train time: '+str(self.train_time))
         return
-    
+        
     def predict(self, joke, user):
         X = self.features[joke]
         Theta = self.coef[user]
 
         pred = np.dot(Theta.T, X)
         return pred
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
         
